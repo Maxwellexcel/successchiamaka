a5f578b1bc0886db34bde2ab2423239f82465998
HAKI <74822382+Maxwellexcel@users.noreply.github.com>
2024-09-06 06:47:54 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+// JavaScript to dynamically load content
+document.getElementById("nickname").textContent = "SUCCESS ODINAKA CHIAMAKA";
+document.getElementById("school-status").textContent = "Currently studying Handmade girls model secondary school";
+document.getElementById("ambition").textContent = "Aspiring Software and Mechatronics Engineer who loves robots!";
+document.getElementById("age").textContent = "Age: 15"; // Example
